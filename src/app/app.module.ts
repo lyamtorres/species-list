bcb506c612119c3b38613ce23e9c8fb9a2cc2dab
lyamtorres <lyamtorres@hotmail.com>
2022-02-09 19:27:26 +0100

    Minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatRadioModule } from '@angular/material/radio';
 import { ListeEspecesComponent } from './components/liste-especes/liste-especes.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
@@ -19,11 +17,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatRadioModule,
     HttpClientModule,
     MatListModule,
     MatInputModule,
-    MatAutocompleteModule,
     FormsModule,
     ReactiveFormsModule
   ],
